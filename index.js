251c7daa224d9a9475ac92c3706296ef7dbd427e
musicode <musicode@qq.com>
2016-07-04 11:25:46 +0800

    update

--- index.js
+++ index.js
@@ -1,32 +1,55 @@
 
-export default function objectIsChange(newObject, oldObject) {
-    for (let key in newObject) {
+function isObject(obj) {
+    return obj && typeof obj === 'object';
+}
 
-        let newValue = newObject[key];
-        let oldValue = oldObject[key];
-
-        if (newValue !== oldValue) {
-
-            let type = typeof newValue;
-
-            if (type === 'string'
-            || type === 'number'
-            || type === 'boolean'
-            ) {
-                return true;
-            }
-
-            try {
-                newValue = JSON.stringify(newValue);
-                oldValue = JSON.stringify(oldValue);
-                if (newValue !== oldValue) {
-                  return true;
-                }
-            }
-            catch (e) {
-                return true;
-            }
-        }
+function isObjectChange(object1, object2, recursive) {
+    var aKeys = Object.keys(object1).sort();
+    var bKeys = Object.keys(object2).sort();
+    if (isArrayChange(aKeys, bKeys)) {
+        return true;
     }
-    return false;
+    return aKeys.some(
+        function (key) {
+            var aValue = object1[key];
+            var bValue = object2[key];
+
+            // 对于对象或者数组
+            // 优先进行子项对比
+            // typeof array === 'object' 所以先测试数组
+            if (Array.isArray(aValue) && Array.isArray(bValue)) {
+                return isArrayChange(aValue, bValue);
+            }
+            else if (recursive && isObject(aValue) && isObject(bValue)) {
+                return isObjectChange(aValue, bValue);
+            }
+
+            return aValue !== bValue;
+        }
+    );
+}
+
+function isArrayChange(array1, array2){
+    if (array1.length !== array2.length) {
+        return true;
+    }
+    return array1.some(
+        function (item, index) {
+            return item !== array2[index];
+        }
+    );
+}
+
+export default function objectIsChange(newObject, oldObject) {
+
+    if (newObject === oldObject) {
+      return false;
+    }
+
+    if (!isObject(newObject) || !isObject(oldObject)) {
+        return true;
+    }
+
+    return isObjectChange(newObject, oldObject, true);
+
 }
